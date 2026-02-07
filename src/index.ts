c89691fa35bd5d9b06d24b8f1c691c44bb2979a1
Chris <christian.striggow@outlook.com>
2026-02-06 22:17:09 -0800

    chore: initial VC-CMV MVP scaffold

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log("Ventilator Simulator: Online");
+
+const targetVolume = 500; // mL
+const currentVolume = 0;
+
+console.log(`Starting volume: ${currentVolume}mL`);
+console.log(`Target: ${targetVolume}mL`);
